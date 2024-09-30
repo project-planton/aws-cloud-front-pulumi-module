44cfa25371ccc0704937f4d7ed42822c797eef50
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-09-30 21:02:15 +0530

    using project-planton apis

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	awscloudfrontv1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/apis/provider/aws/awscloudfront/v1"
 	"github.com/pkg/errors"
 	"github.com/plantoncloud/aws-cloud-front-pulumi-module/pkg"
-	"github.com/plantoncloud/project-planton/apis/zzgo/cloud/planton/apis/code2cloud/v1/aws/awscloudfront"
 	_ "github.com/plantoncloud/project-planton/apis/zzgo/cloud/planton/apis/iac/v1/stackjob"
 	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/stackinput"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		stackInput := &awscloudfront.AwsCloudFrontStackInput{}
+		stackInput := &awscloudfrontv1.AwsCloudFrontStackInput{}
 
 		if err := stackinput.LoadStackInput(ctx, stackInput); err != nil {
 			return errors.Wrap(err, "failed to load stack-input")
