dc9b0bd5cc3cf112d1a170fe79c1b8a204e8dd52
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-10-02 10:55:04 +0530

    removed apis prefix in the package name for project-planton generated deps

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	awscloudfrontv1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/apis/provider/aws/awscloudfront/v1"
+	awscloudfrontv1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/provider/aws/awscloudfront/v1"
 	"github.com/pkg/errors"
 	"github.com/plantoncloud/aws-cloud-front-pulumi-module/pkg"
-	_ "github.com/plantoncloud/project-planton/apis/zzgo/cloud/planton/apis/iac/v1/stackjob"
 	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/stackinput"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
