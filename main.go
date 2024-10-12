84155b51a4c6bd2ceb3f30e2425d3a4fe1cc3e51
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-10-12 16:52:44 +0530

    moved to project-planton

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	awscloudfrontv1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/provider/aws/awscloudfront/v1"
+	awscloudfrontv1 "buf.build/gen/go/project-planton/apis/protocolbuffers/go/project/planton/provider/aws/awscloudfront/v1"
 	"github.com/pkg/errors"
-	"github.com/plantoncloud/aws-cloud-front-pulumi-module/pkg"
-	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/stackinput"
+	"github.com/project-planton/aws-cloud-front-pulumi-module/pkg"
+	"github.com/project-planton/pulumi-module-golang-commons/pkg/stackinput"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
